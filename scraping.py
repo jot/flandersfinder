1ec84acbb1404db4807d580392d3b6439935c819
Jacob <jacob.maresca@googlemail.com>
2015-11-20 15:53:45 +0000

    scraping code from NED directory

---
+++ scraping.py
@@ -0,0 +1,48 @@
+import urllib2
+from bs4 import BeautifulSoup
+import csv
+
+def deg2HMS(ra='', dec='', round=False):
+  RA, DEC, rs, ds = '', '', '', ''
+  if dec:
+    if str(dec)[0] == '-':
+      ds, dec = '-', abs(dec)
+    deg = int(dec)
+    decM = abs(int((dec-deg)*60))
+    if round:
+      decS = int((abs((dec-deg)*60)-decM)*60)
+    else:
+      decS = (abs((dec-deg)*60)-decM)*60
+    DEC = '{0}{1} {2} {3}'.format(ds, deg, decM, decS)
+  
+  if ra:
+    if str(ra)[0] == '-':
+      rs, ra = '-', abs(ra)
+    raH = int(ra/15)
+    raM = int(((ra/15)-raH)*60)
+    if round:
+      raS = int(((((ra/15)-raH)*60)-raM)*60)
+    else:
+      raS = ((((ra/15)-raH)*60)-raM)*60
+    RA = '{0}{1} {2} {3}'.format(rs, raH, raM, raS)
+  
+  if ra and dec:
+    return (RA, DEC)
+  else:
+    return RA or DEC
+
+with open('WP8_photoz_COSMOS.csv','rb') as csvfile:
+    reader = csv.reader(csvfile,delimiter=',')
+    for row in reader:
+        if row > 0:
+            print row
+            #posi = deg2HMS(row[0],row[1])
+        #url = 'https://ned.ipac.caltech.edu/cgi-bin/objsearch?in_csys=Equatorial&in_equinox=J2000.0&lon=%s&lat=%s&radius=0.1&hconst=73&omegam=0.27&omegav=0.73&corr_z=1&search_type=Near+Position+Search&z_constraint=Unconstrained&z_value1=&z_value2=&z_unit=z&ot_include=ANY&nmp_op=ANY&out_csys=Equatorial&out_equinox=J2000.0&obj_sort=Distance+to+search+center&of=pre_text&zv_breaker=30000.0&list_limit=5&img_stamp=YES' % (in0, in1)
+        #page = urllib2.urlopen(url)
+        #soup = BeautifulSoup(page,"html.parser")
+        #content1 = soup.p
+        #print content1
+    
+
+
+
