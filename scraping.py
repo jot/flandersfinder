d3e3f44e0ddf53a444f9dd2fd313d877c0bd8993
Jacob <jacob.maresca@googlemail.com>
2015-11-20 16:05:25 +0000

    add csv

--- scraping.py
+++ scraping.py
@@ -35,8 +35,7 @@ with open('WP8_photoz_COSMOS.csv','rb') as csvfile:
     reader = csv.reader(csvfile,delimiter=',')
     for row in reader:
         if row > 0:
-            print row
-            #posi = deg2HMS(row[0],row[1])
+            posi = deg2HMS(row[0],row[1])
         #url = 'https://ned.ipac.caltech.edu/cgi-bin/objsearch?in_csys=Equatorial&in_equinox=J2000.0&lon=%s&lat=%s&radius=0.1&hconst=73&omegam=0.27&omegav=0.73&corr_z=1&search_type=Near+Position+Search&z_constraint=Unconstrained&z_value1=&z_value2=&z_unit=z&ot_include=ANY&nmp_op=ANY&out_csys=Equatorial&out_equinox=J2000.0&obj_sort=Distance+to+search+center&of=pre_text&zv_breaker=30000.0&list_limit=5&img_stamp=YES' % (in0, in1)
         #page = urllib2.urlopen(url)
         #soup = BeautifulSoup(page,"html.parser")
